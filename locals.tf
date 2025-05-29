91c7e0620b5606a85a424aff4623147d8b50faac
Elijah <ebwright@protonmail.com>
2025-05-29 18:45:24 +0100

    updated: locals

--- locals.tf
+++ locals.tf
@@ -26,16 +26,37 @@ locals {
     )
   }
 
-  join_domain_settings = {
+  # join_domain_settings = {
+  #   Name    = var.domain_name
+  #   OUPath  = var.domain_ou_path
+  #   User    = var.domain_join_username
+  #   Restart = "true"
+  #   Options = "3"
+  # }
+  # join_domain_password_value = length(data.azurerm_key_vault_secret.domain_join_secret) > 0 ? data.azurerm_key_vault_secret.domain_join_secret[0].value : null
+  # join_domain_protected_settings = local.join_domain_password_value != null ? {
+  #   Password = data.azurerm_key_vault_secret.domain_join_secret
+  # }: null
+
+   join_domain_password_value = (
+    local.is_ad_join &&
+    length(data.azurerm_key_vault_secret.domain_join_secret) > 0
+  ) ? data.azurerm_key_vault_secret.domain_join_secret[0].value : null
+
+  join_domain_settings = local.is_ad_join ? {
     Name    = var.domain_name
     OUPath  = var.domain_ou_path
     User    = var.domain_join_username
     Restart = "true"
     Options = "3"
-  }
-  join_domain_password_value = length(data.azurerm_key_vault_secret.domain_join_secret) > 0 ? data.azurerm_key_vault_secret.domain_join_secret[0].value : null
-  join_domain_protected_settings = local.join_domain_password_value != null ? {
-    Password = data.azurerm_key_vault_secret.domain_join_secret
-  }: null
+  } : null
+
+  join_domain_protected_settings = (
+    local.is_ad_join && local.join_domain_password_value != null
+  ) ? {
+    Password = local.join_domain_password_value
+  } : null
+
+
 
 }
