66903019697b692f0a566917c675ba9dbb9eb530
Elijah <ebwright@protonmail.com>
2025-05-29 19:56:07 +0100

    added: automatic platform updates to true

--- main.tf
+++ main.tf
@@ -103,6 +103,7 @@ resource "azurerm_windows_virtual_machine" "avd_host_vm" {
   #vm_agent_platform_updates_enabled = true
   #patch_assessment_mode             = "AutomaticByPlatform"
   patch_mode = "AutomaticByOS"
+  vm_agent_platform_updates_enabled = true
 
   identity {
     type = "SystemAssigned"
